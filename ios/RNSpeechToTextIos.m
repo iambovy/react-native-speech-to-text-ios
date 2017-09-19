03f3379bf3eb31dae43334f34dc10933243c5eb3
bovy gaysorn <bovygaysorn@bovys-MacBook-Air.local>
2017-09-19 10:59:13 +0700

    fixed return result

--- ios/RNSpeechToTextIos.m
+++ ios/RNSpeechToTextIos.m
@@ -114,7 +114,7 @@ RCT_EXPORT_METHOD(startRecognition:(NSString*)localeStr)
     self.inputNode = self.audioEngine.inputNode;
 
     self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
-    self.recognitionRequest.shouldReportPartialResults = YES;
+    self.recognitionRequest.shouldReportPartialResults = NO;
 
     AVAudioFormat *format = [self.inputNode outputFormatForBus:0];
 
